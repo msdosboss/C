6dff8f4566e59ec7841a085376f54580aff0799d
msdosboss <jacobt0722@gmail.com>
2022-04-06 16:45:55 -0700

    chess
    bishop can move but not capture

--- chess.c
+++ chess.c
@@ -17,8 +17,8 @@ int main(){
         {50,20,30,90,100,30,20,50},                                          // 50 = white rook, 20 = white knight, 30 = white bishop, 90 = white queen, 100 = white king, 10 = white pawn, 51 = black rook, 21 = black knight, 31 = black bishop, 91 = black queen, 101 = black king, 11 = black pawn
         {10,10,10,10,10,10,10,10},
         {0,0,0,0,0,0,0,0},
-        {0,0,0,30,0,0,0,0},
         {0,0,0,0,0,0,0,0},
+        {0,0,0,0,30,0,0,0},
         {0,0,0,0,0,0,0,0},
         {11,11,11,11,11,11,11,11},
         {51,21,31,91,101,31,21,51},
@@ -297,47 +297,52 @@ int bishop(int board[][8],int *currentsquare,int color){
     squarecord2 = (int *) malloc(5);
 
     for(int i = 0; i + (*currentsquare + 1) <= 7 && i + *currentsquare <= 7; i++){
-        printf(" i = %d",i);
         if(board[*(currentsquare + 1) + i + 1][*currentsquare + i + 1] > 0){
-            printf(" test ");
-            upright = currentsquare;
-           *upright = i + *upright;
-           *(upright + 1) = i + *(upright + 1);                         //upright is now the right and up most square you can move
-            printf(" i = %d",i);
-            printf("\nupright = %d %d\n",*(upright + 1), *upright);
+            *upright = *currentsquare;
+            *(upright + 1) = *(currentsquare + 1);
+            *upright = i + *upright;
+            *(upright + 1) = i + *(upright + 1);                         //upright is now the right and up most square you can move
             break;
         }
     }
     for(int i = 0; (*currentsquare + 1) - i >= 0 && i + *currentsquare <= 7; i++){
-        if(board[*(currentsquare + 1) - i][*currentsquare + i] > 0){
-            downright = currentsquare;
+        if(board[(*(currentsquare + 1) - i) - 1][*currentsquare + i + 1] > 0){
+            *downright = *currentsquare;
+            *(downright + 1) = *(currentsquare + 1);
            *downright = i + *downright;
            *(downright + 1) = (*(downright + 1) - i);                         //down is now the right and down most square you can move
             break;
         }
     }
     for(int i = 0; (*currentsquare + 1) - i >= 0 && *currentsquare - i >= 0; i++){
-        if(board[*(currentsquare + 1) - i][*currentsquare - i] > 0){
-            printf("test 4");
-            downleft = currentsquare;
-           *downleft = (*downleft - i);
-           *(downleft + 1) = (*(downleft + 1) - i);                         //down is now the left and down most square you can move
+        if(board[(*(currentsquare + 1) - i) - 1][(*currentsquare - i) - 1] > 0){
+            *downleft = *currentsquare;
+            *(downleft + 1) = *(currentsquare + 1);
+            *downleft = (*downleft - i);
+            *(downleft + 1) = (*(downleft + 1) - i);                         //down is now the left and down most square you can move
             break;
         }
     }
         for(int i = 0; i + (*currentsquare + 1) <= 7 && *currentsquare - i >= 0; i++){
-        if(board[*(currentsquare + 1) + i][*currentsquare - i] > 0){
-            upleft = currentsquare;
-           *upleft = (*upleft - i);
-           *(upleft + 1) = i + *(upleft + 1);                         //upleft is now the left and up most square you can move
+        if(board[*(currentsquare + 1) + i + 1][(*currentsquare - i) - 1] > 0){
+            *(upleft + 1) = *(currentsquare + 1);
+            *upleft = *currentsquare;
+            *upleft = (*upleft - i);
+            *(upleft + 1) = i + *(upleft + 1);                         //upleft is now the left and up most square you can move
             break;        
         }
     }
-    printf("upleft %d %d\n",*(upleft +1),*upleft);
+    printf("\nupleft %d %d\n",*(upleft +1),*upleft);
     printf("upright %d %d\n",*(upright +1),*upright);
     printf("downleft %d %d\n",*(downleft + 1),*downleft);
     printf("downright %d %d\n",*(downright +1),*downright);
     
     printf(" where would you like to move your Bishop? ");
-    squarecord2 = getmove();    
+    squarecord2 = getmove();
+    for(int i = 0;i + *(currentsquare + 1) <=7 && i + *currentsquare <=7; i++){
+        if(*squarecord2 == (*downleft + i) && *(squarecord2 + 1) == *(downleft) + i){
+            board[*(squarecord2 + 1)][*squarecord2] = board[*(currentsquare + 1)][*currentsquare];
+            board[*(currentsquare + 1)][*currentsquare] = 0;
+        }
+    }    
 }
